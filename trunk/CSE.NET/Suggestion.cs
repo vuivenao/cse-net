6ade9ecb458c0390f517a4db0dafd560fdf8dd90
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-10 21:22:37 +0000

    Suggestion added

---
+++ trunk/CSE.NET/Suggestion.cs
@@ -0,0 +1,24 @@
+﻿namespace Google.CustomSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Suggestion
+    {
+        public Suggestion()
+        {
+        }
+
+        public Suggestion(string label, string query)
+        {
+            this.Label = label;
+            this.Query = query;
+        }
+
+        public string Label { get; internal set; }
+
+        public string Query { get; internal set; }
+    }
+}
