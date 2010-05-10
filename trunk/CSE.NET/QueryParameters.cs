235e38d2a238ab597f556c36144ff70c6729645d
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-10 11:19:07 +0000

    Set default value for QueryParameters.Format to XML_NO_DTD

--- trunk/CSE.NET/QueryParameters.cs
+++ trunk/CSE.NET/QueryParameters.cs
@@ -8,11 +8,21 @@ namespace Google.CustomSearch
     public class QueryParameters
     {
         public int Start { get; set; }
+
         public int Count { get; set; }
+
         public string SearchTerm { get; set; }
+
+        public Facet Filter { get; set; }
+
         public OutputFormat Format { get; set; }
 
         // TODO: Possibly swap encoding for a built in type
         public string Encoding { get; set; }
+
+        public QueryParameters()
+        {
+            this.Format = OutputFormat.XmlNoDtd;
+        }
     }
 }
