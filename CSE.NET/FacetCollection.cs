2f8fb2bb83ab4c824eb1e93f285213871172bb09
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-10 10:25:14 +0000

    - SearchResult object added - Facet and FacetCollection added - Updated the ResponseParser to parse various properties from the <GSP>, <Context> and <RES> tags - Updated the test rig to utilise some of these properties

---
+++ CSE.NET/FacetCollection.cs
@@ -0,0 +1,27 @@
+﻿namespace Google.CustomSearch
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    public class FacetCollection : Dictionary<string, string>
+    {
+        public new void Add(string label, string anchor)
+        {
+            base.Add(label, anchor);
+        }
+
+        public void Add(Facet facet)
+        {
+            this.Add(facet.Label, facet.AnchorText);
+        }
+
+        public new string this[string label]
+        {
+            get
+            {
+                return base[label];
+            }
+        }
+    }
+}
---
+++ CSE.NET/SearchResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Google.CustomSearch
+{
+    public class SearchResult
+    {
+        public int StartIndex { get; internal set; }
+
+        public int EndIndex { get; internal set; }
+
+        public int Total { get; internal set; }
+
+        public string Time { get; internal set; }
+
+        public string Title { get; internal set; }
+
+        public Uri NextPageLink { get; internal set; }
+
+        public Uri PreviousPageLink { get; internal set; }
+
+        public ResultCollection Results { get; private set; }
+
+        public FacetCollection Facets { get; private set; }
+
+        public QueryParameters Parameters { get; internal set; }
+
+        internal SearchResult()
+        {
+            this.Facets = new FacetCollection();
+            this.Results = new ResultCollection();
+        }
+
+        internal SearchResult(QueryParameters parameters)
+            : this()
+        {
+            this.Parameters = parameters;
+        }
+
+        public SearchResult(ResultCollection results)
+        {
+            this.Results = results;
+        }
+    }
+}
