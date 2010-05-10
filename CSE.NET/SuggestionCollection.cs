a8db69432d96e583ef222e9ce4a34783c9a89670
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-10 20:35:48 +0000

    - Class diagram added - SuggestionCollection class added - Suggestions property added to SearchResult

---
+++ CSE.NET/SuggestionCollection.cs
@@ -0,0 +1,25 @@
+﻿namespace Google.CustomSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SuggestionCollection
+    {
+        public string Label { get; internal set; }
+
+        public string Query { get; internal set; }
+
+        public SuggestionCollection()
+        {
+
+        }
+
+        public SuggestionCollection(string label, string query)
+        {
+            this.Label = label;
+            this.Query = query;
+        }
+    }
+}
