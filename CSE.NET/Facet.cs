2dc938dbec617fcf8ba7a197ce8f5fb66d3c70a2
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-10 16:15:45 +0000

    - Exact result count implemented - GetAvailableCount implemented in Facet

--- CSE.NET/Facet.cs
+++ CSE.NET/Facet.cs
@@ -10,12 +10,34 @@
 
         public string AnchorText { get; internal set; }
 
-        public Facet() { }
+        public QueryParameters Parameters { get; internal set; }
+
+        internal Facet()
+        {
+        }
+
+        public Facet(string label)
+        {
+            this.Label = label;
+        }
+
+        public Facet(QueryParameters parameters)
+        {
+            this.Parameters = parameters;
+        }
 
         public Facet(string label, string anchorText)
         {
             this.Label = label;
             this.AnchorText = anchorText;
+        }
+
+        public int GetAvailableCount()
+        {
+            QueryParameters temp = this.Parameters.Clone();
+            temp.Count = 100;
+            temp.Filter = this;
+            return new CustomSearchClient().Search(temp).Total;
         }
     }
 }
