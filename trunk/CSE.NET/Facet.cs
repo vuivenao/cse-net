8057acb6b660c17d0bfda1e63c67631a55bc1850
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-12 12:22:27 +0000

    Documentation comments added to the Facet class

--- trunk/CSE.NET/Facet.cs
+++ trunk/CSE.NET/Facet.cs
@@ -1,33 +1,73 @@
 ﻿namespace Google.CustomSearch
 {
+    using System;
+
+    /// <summary>
+    /// Facet objects encapsulate information about a refinement label associated with a set of search results.
+    /// </summary>
     public class Facet
     {
+        /// <summary>
+        /// Initializes a new instance of the Google.CustomSearch.Facet class specifying the label.
+        /// </summary>
+        /// <param name="label">The label of this Facet.</param>
         public Facet(string label)
         {
             this.Label = label;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the Google.CustomSearch.Facet class specifying the 
+        /// QueryParameters that were used to generate the search to which this Facet is related.
+        /// </summary>
+        /// <param name="parameters">The QueryParameters to which this Facet is related.</param>
         public Facet(QueryParameters parameters)
         {
             this.Parameters = parameters;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the Google.CustomSearch.Facet class specifying the label
+        /// and the anchor text of this Facet.
+        /// </summary>
+        /// <param name="label">The label of this Facet.</param>
+        /// <param name="anchorText">The anchor text of this Facet.</param>
         public Facet(string label, string anchorText)
         {
             this.Label = label;
             this.AnchorText = anchorText;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the Google.CustomSearch.Facet class with no information specified.
+        /// </summary>
         internal Facet()
         {
         }
 
+        /// <summary>
+        /// Gets the refinement label that you can use to filter the search results that you receive.
+        /// </summary>
         public string Label { get; internal set; }
 
+        /// <summary>
+        /// Gets the text that you should display to users to identify a refinement label associated with a search result set.
+        /// Since refinement Facet.Label replaces nonalphanumeric characters with underscores, 
+        /// you should not display the value of the Facet.Label in your user interface. 
+        /// Instead, you should display the value of the Facet.AnchorText.
+        /// </summary>
         public string AnchorText { get; internal set; }
 
+        /// <summary>
+        /// Gets the QueryParameters that were used to generate the search to which this Facet is related.
+        /// </summary>
         public QueryParameters Parameters { get; internal set; }
 
+        /// <summary>
+        /// Gets the number of results available for the current Facet object.
+        /// </summary>
+        /// <returns>Returns the number of available results for this Facet.</returns>
+        [Obsolete("I Dont think this should be used any more, maybe")]
         public int GetAvailableCount()
         {
             QueryParameters temp = this.Parameters.Clone();
