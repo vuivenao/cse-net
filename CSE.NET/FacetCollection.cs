3efbb8a39ac276a777dd92d6f52a8966d0ac3b39
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-12 12:32:03 +0000

    Documentation comments added to FacetCollection

--- CSE.NET/FacetCollection.cs
+++ CSE.NET/FacetCollection.cs
@@ -3,8 +3,16 @@
     using System.Collections.ObjectModel;
     using System.Linq;
 
+    /// <summary>
+    /// A collection of Google.CustomSearch.Facet objects.
+    /// </summary>
     public class FacetCollection : Collection<Facet>
     {
+        /// <summary>
+        /// Gets a Facet contained in the collection.
+        /// </summary>
+        /// <param name="label">The label of the Facet to get.</param>
+        /// <returns>Returns a Facet or null.</returns>
         public Facet this[string label]
         {
             get
@@ -13,11 +21,21 @@
             }
         }
 
+        /// <summary>
+        /// Adds an object to the end of the Google.CustomSearch.FacetCollection.
+        /// </summary>
+        /// <param name="label">The label of the Facet to add to the collection.</param>
+        /// <param name="anchor">The anchor text of the Facet to add to the collection.</param>
         public void Add(string label, string anchor)
         {
             base.Add(new Facet(label, anchor));
         }
 
+        /// <summary>
+        /// Determines whether an element is in the Google.CustomSearch.FacetCollection.
+        /// </summary>
+        /// <param name="label">The label of the Google.CustomSearch.Facet to locate.</param>
+        /// <returns>Returns true if Facet is found; otherwise, false.</returns>
         public bool Contains(string label)
         {
             foreach (var item in this)
@@ -25,18 +43,5 @@
 
             return false;
         }
-
-        //public void Add(Facet facet)
-        //{
-        //    this.Add(facet.Label, facet.AnchorText);
-        //}
-
-        //public new string this[string label]
-        //{
-        //    get
-        //    {
-        //        return base[label];
-        //    }
-        //}
     }
 }
