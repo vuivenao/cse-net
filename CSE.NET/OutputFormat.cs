9d37993d8a81929f0f7a5a4406b2e804f2c9197b
greglio.GB <greglio.GB@57191c00-1c07-af22-fbad-0a461fac0c27>
2010-05-19 07:59:54 +0000

    Documentation comments added

--- CSE.NET/OutputFormat.cs
+++ CSE.NET/OutputFormat.cs
@@ -1,8 +1,20 @@
 ﻿namespace Google.CustomSearch
 {
+    /// <summary>
+    /// The valid values for the output format parameter of a CSE request.
+    /// </summary>
     public enum OutputFormat
     {
+        /// <summary>
+        /// <para>The XML results will contain a Google DTD reference. 
+        /// The second line of the result will identify the document definition type (DTD) that the results use:</para>
+        /// &lt;!DOCTYPE GSP SYSTEM "google.dtd"&gt;
+        /// </summary>
+        /// <example>&lt;!DOCTYPE GSP SYSTEM "google.dtd"&gt;</example>
         Xml,
+        /// <summary>
+        /// The XML results will not include a !DOCTYPE statement (recommended).
+        /// </summary>
         XmlNoDtd
     }
 }
